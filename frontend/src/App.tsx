ee160e2bcd75cbc7f92684665a1560be22cc284d
Dohyun Kim <0704dohyun@gmail.com>
2024-08-09 00:39:13 +0900

    chore: add title and favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import { Helmet } from "react-helmet";
+import iconUrl from "./assets/favicon.ico";
 
 function Logout() {
   localStorage.clear();
@@ -12,22 +14,28 @@ function Logout() {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Helmet>
+        <title>Django Notes</title>
+        <link rel="icon" type="image/x-icon" href={iconUrl}></link>
+      </Helmet>
+    </>
   );
 }
 
