2844f655f95c298712177f3545ff7a2ee806637d
Dohyun Kim <0704dohyun@gmail.com>
2024-08-09 00:13:28 +0900

    feat(backend): build a note-taking api with auth

---
+++ backend/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('notes/', views.NoteListCreate.as_view(), name='list_notes'),
+    path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='delete_notes'),
+]
